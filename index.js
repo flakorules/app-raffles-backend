f22a0b93520286f6f8a06ca74dc787274fbe9821
flakorules <flakorules@gmail.com>
2021-11-17 17:54:24 -0300

    Se agrega directorio publico

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { dbConnection } = require("./database/config");
 const app = express();
 dbConnection();
 app.use(cors());
-
+app.use(express.static("public"));
 app.use(express.json());
 
 app.get("/", (req, res) => {
